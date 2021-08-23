28172969ecfcac0611eb56a237e8afa6c715d9f2
Ekpa <ekpa.ntan@gmail.com>
2021-08-23 12:45:48 +0100

    text slider added

--- scripts/index.js
+++ scripts/index.js
@@ -1,53 +1,24 @@
-const detailSections = Array.from(document.getElementsByClassName('detail-section'));
-const headerSections= Array.from(document.getElementsByClassName('main-section'));
+const stacks = ["Frontend-Developer", "Backend-Developer", "Incredibly Fun Guy", "Absolute Team Player"]
+const stackHolder = document.getElementById('my-Stack')
 
-console.log(detailSections)
 
-const rightIndex = (index) => {
-    return detailSections[index];
+
+let stackIndex = 0;
+const stackLooper = () => {
+    // debugger
+    for(i=0; i<stacks.length; i++) {
+        
+}
+stackIndex ++;
+if (stackIndex > stacks.length) {
+    stackIndex = 1;
+}
+let something = stackIndex-1
+stackHolder.innerText = stacks[something]
+setTimeout(stackLooper, 2000);
 }
 
-
-const detailShow = (e, index, header) => {
-    detailSections.map((detail, detailIndex) => {
-        if (detailIndex === index && detail.classList.contains('detail-section')) {
-            detail.classList.remove('detail-section')
-            
-            
-        } else if (detailIndex === index) {
-            detail.classList.add('detail-section')
-            
-            
-        }
-    })
-}
-
-// const windowListener = () => {   
-//     console.log(window.innerHeight) 
-//     if (window.innerWidth < 616) {
-//     headerSections.forEach ((headerSection, index) => {
-//         // const details = rightIndex(index)
-//        headerSection.addEventListener('click', (e) => detailShow(e, index, headerSection))
-//     })
-//     } else if(window.innerWidth > 615) {
-//         headerSections.forEach ((headerSection, index) => {
-//            headerSection.removeEventListener('click', detailShow)
-//         })
-//     }
-// }
-
-
-// window.addEventListener('resize', windowListener);
-
-headerSections.forEach ((headerSection, index) => {
-    // const details = rightIndex(index)
-   headerSection.addEventListener('click', (e) => detailShow(e, index))
-})
-
-
-
-
-
+stackLooper()
 
 
 
